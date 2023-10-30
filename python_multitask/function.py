97f535879179d2a0a91e54cf8917f6d459f5a369
Алексей Русяев <RusyaevAA@outlook.com>
2023-10-30 11:54:52 +0400

    adding some cosmetic addition

--- python_multitask/function.py
+++ python_multitask/function.py
@@ -24,21 +24,28 @@ def calculating_sum_in_rows_with_multiprocessing(mas):
     return sum(sum2)
 
 def main():
+    print("Enter the number of rows:", end=" ")
     num_of_rows = int(input())
+    print("Enter the number of columns:", end=" ")
     num_of_columns = int(input())
     mas = []
-
+    print("Enter the elements of matrix.")
     for i in range(num_of_rows):
+        print("Enter the elements of %d row:" %(i+1), end=" ")
         mas.append(list(map(int, input().split())))
 
     time = default_timer()
     sum_in_rows = calculating_sum_in_rows_without_multiprocessing(mas)
+    print("Result of calculation numbers in rows of matrix:", end=" ")
     print(sum_in_rows)
+    print("Time of execution:", end=" "),
     print(default_timer() - time)
 
     time = default_timer()
     sum_in_rows = calculating_sum_in_rows_with_multiprocessing(mas)
+    print("Result of calculation numbers in rows of matrix:", end=" ")
     print(sum_in_rows)
+    print("Time of execution:", end=" "),
     print(default_timer() - time)
 
 if __name__ == "__main__":
