161325cc2653f32d58ea280559529a829503b334
Алексей Русяев <RusyaevAA@outlook.com>
2023-10-31 00:02:56 +0400

    remaking for PEP 8, added input from command line

--- python_multitask/function.py
+++ python_multitask/function.py
@@ -3,7 +3,10 @@ from timeit import default_timer
 from time import sleep
 from multiprocessing import Pool
 
-def calculating_sum_in_rows_without_multiprocessing(mas):
+
+def calculating_sum_in_rows_without_multiprocessing(mas: list) -> int:
+    """Считает сумму чисел в строке без параллельных вычислений"""
+
     sum2 = 0
     for row in mas:
         sleep(1)
@@ -13,26 +16,34 @@ def calculating_sum_in_rows_without_multiprocessing(mas):
         sum2 += sum1
     return sum2
 
-def summation(x):
+
+def summation(x: list) -> int:
+    """Суммирование элементов в 1 строке"""
+
     return sum(x)
 
-def calculating_sum_in_rows_with_multiprocessing(mas):
+
+def calculating_sum_in_rows_with_multiprocessing(mas: list) -> int:
+    """Считает сумму чисел в строке при помощи параллельных вычислений"""
+
     sum2 = []
     with Pool(20) as p:
         sleep(1)
         sum2 += p.map(summation, mas)
     return sum(sum2)
 
-def main():
-    print("Enter the number of rows:", end=" ")
-    num_of_rows = int(input())
-    print("Enter the number of columns:", end=" ")
-    num_of_columns = int(input())
+
+def main() -> None:
+
+    num_of_rows = sys.argv[1]
+    num_of_columns = sys.argv[2]
+
     mas = []
-    print("Enter the elements of matrix.")
-    for i in range(num_of_rows):
-        print("Enter the elements of %d row:" %(i+1), end=" ")
-        mas.append(list(map(int, input().split())))
+    for i in range(int(num_of_rows)):
+        mas1 = []
+        for j in range(int(num_of_columns)):
+            mas1.append(int(sys.argv[(i * int(num_of_rows) + j) + 3]))
+        mas.append(mas1)
 
     time = default_timer()
     sum_in_rows = calculating_sum_in_rows_without_multiprocessing(mas)
@@ -47,6 +58,3 @@ def main():
     print(sum_in_rows)
     print("Time of execution:", end=" "),
     print(default_timer() - time)
-
-if __name__ == "__main__":
-    main()
